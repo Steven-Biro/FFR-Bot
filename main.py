c32cc8ad621c0ea9e9264875146bc78520e1c777
Steven <superusersteven@gmail.com>
2019-01-07 14:19:57 -0500

    wip

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 import re
 import time
-import sqlite3
+import pickle
 from datetime import datetime, timedelta
 import weekly
 import urllib.request
@@ -14,57 +14,146 @@ from discord.utils import get
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
+    level=logging.DEBUG,
     datefmt='%Y-%m-%d %H:%M:%S')
 
 description = 'FFR discord bot'
 
+admins = []
 bot = commands.Bot(command_prefix='?', description=description)
-
-
-# constants
-Sleep_Time = 5000
-
-# challenge = weekly.Weekly(category = "Challenge Seed", role="challengeseed", adminrole="challengeseedadmin", seedchannel="challengeseed", spoilerchannel="challengeseedspoilerchat", leaderboardchannel="challengeseedleaderboard")
-challenge = weekly.Weekly()
-challenge.load("Challenge Seed")
-
-
-# asyncseed = weekly.Weekly(category = "Weekly-Async", role="asyncseed", adminrole="asynseedasmin", seedchannel="async-seed_flags", spoilerchannel="async-spoilers", leaderboardchannel="async-leaderboard")
-asyncseed = weekly.Weekly()
-asyncseed.load("Weekly-Async")
-
 Weeklies = dict()
-Weeklies[challenge.get("category")] = challenge
-Weeklies[asyncseed.get("category")] = asyncseed
 
-@bot.command()
-async def wipeleaderboard(ctx, name: str):
-    await Weeklies[str(ctx.channel.category())].wipeleaderboard(name)
-    await ctx.channel.purge(1)
+def is_admin(ctx):
+    user = ctx.author
+    return (user.id in admins) or ctx.bot.is_owner(user)
 
-@bot.command()
-async def createleaderboard(ctx, name: str,  num: int, *args: str):
-    await Weeklies[str(ctx.channel.category())].initializeleaderboard(name, num, *args)
-    await ctx.channel.purge(1)
+@bot.command(pass_context=True)
+@commands.check(is_admin)
+async def load(ctx):
+    _load()
+
+@bot.command(pass_context=True)
+@commands.check(is_admin)
+async def save(ctx):
+    _save()
+
+
+def _load():
+    global admins, Weeklies
+    with open('data', 'rb') as f:
+        data = pickle.load(f)
+        Weeklies = data[0]
+        admins = data[1]
+def _save():
+    with open('data', 'wb') as f:
+        data = [Weeklies,admins]
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+
+@bot.command(pass_context=True)
+@commands.check(is_admin)
+async def set(ctx, variable, value):
+
+    if variable is "admin":
+        mentions = ctx.message.mentions
+        if len(mentions) != 1:
+            return
+        else:
+            admins.append(mentions[0].id)
+    else:
+        Weeklies[ctx.message.category.name].set(variable, value)
 
 
 @bot.command(pass_context=True)
-async def multi(ctx, raceid: str = None):
+@commands.check(is_admin)
+async def createweekly(ctx, *args):
+    values = list(args)
+    for i in range(len(args)):
+        if args[i][1:2] == '#':
+            values[i] = get(ctx.guild.channels, id=int(re.sub('[^0-9]', '', args[i])))
+        else:
+            values[i] = get(ctx.guild.roles, id=int(re.sub('[^0-9]', '', args[i])))
+    Weeklies[ctx.channel.category.name] =\
+        weekly.Weekly(server = ctx.guild, category = ctx.channel.category.name, role=values[0], adminrole=values[1],
+                      seedchannel=values[2], spoilerchannel=values[3],leaderboardchannel=values[4])
+
+@bot.command(pass_context=True)
+async def wipeleaderboard(ctx, name: str):
+    leaderboardchannel = await Weeklies[str(ctx.channel.category.id)].get("leaderboard")
+    message = await leaderboardchannel.send("initializing...")
+    await Weeklies[str(ctx.channel.category.id)].wipeleaderboard(name, message, ctx.author.roles)
+    await message.edit(Weeklies[str(ctx.channel.category.id)].writeleaderboard())
+    await ctx.channel.purge(1)
+
+@bot.command(pass_context=True)
+async def createleaderboard(ctx, name: str,  num: int = 1, *args: str):
+    leaderboardchannel = Weeklies[str(ctx.channel.category.name)].get("leaderboard")
+    print(leaderboardchannel)
+    print(type(leaderboardchannel))
+    message = await leaderboardchannel.send("initializing...")
+    await Weeklies[str(ctx.channel.category.id)].initializeleaderboard(name, message, ctx.author.roles, num, *args)
+    await message.edit(Weeklies[str(ctx.channel.category.id)].writeleaderboard())
+    await ctx.channel.purge(1)
+
+@bot.command(pass_context=True)
+async def submit(ctx, runnertime: str = None, board_num: int = 0):
+    await ctx.channel.purge(1)
+    if runnertime is None:
+        await ctx.author.send("You must include a time when you submit a time.")
+        return
+
+    if (board_num > Weeklies[str(ctx.channel.category.id)].get("board_num")) or (board_num < 0):
+        await ctx.author.send("You must select a valid board to submit a time for.")
+        return
+    try:
+        # convert to seconds using this method to make sure the time is readable and valid
+        # also allows for time input to be lazy, ie 1:2:3 == 01:02:03 yet still maintain a consistent
+        # style on the leaderboard
+        t = datetime.strptime(runnertime, "%H:%M:%S")
+    except ValueError:
+        await ctx.author.send("The time you provided '" + str(runnertime) +
+                               "', this is not in the format HH:MM:SS (or you took a day or longer)")
+        return
+
+
+    role = await Weeklies[str(ctx.channel.category.id)].addtoleaderboard(ctx.author.id,ctx.author.display_name,t,ctx.author.roles,board_num=board_num)
+    board = await Weeklies[str(ctx.channel.category.id)].writeleaderboard()
+    message = await Weeklies[str(ctx.channel.category.id)].get("leaderboardmessage")
+    message.edit(board)
+
+    await ctx.author.add_roles(role, "Automatic, for a weekly seed")
+
+@bot.command(pass_context=True)
+async def remove(ctx, board_num, *args):
+    players = ctx.message.mentions
+    for player in players:
+        role = await Weeklies[str(ctx.channel.category.id)].removefromleaderboard(player, ctx.author.id, board_num=board_num)
+        await ctx.author.remove_roles(role, "Automatic, for a weekly seed, must be a weekly seed admin to use")
+
+@bot.command(pass_context=True)
+async def spectate(ctx):
+    role = await ctx.author.add_roles(Weeklies[str(ctx.channel.category.id)].spectate(ctx.author.id,ctx.author.roles))
+    await ctx.author.add_roles(role, "Automatic, for a weekly seed")
+
+@bot.command(pass_context=True)
+async def forfeit(ctx, board_num = 0):
+    role = await ctx.author.add_roles(Weeklies[str(ctx.channel.category.id)].forfeit(ctx.author.id,ctx.author.roles))
+    await ctx.author.add_roles(role, "Automatic, for a weekly seed")
+
+@bot.command(pass_context=True)
+async def multi(ctx, raceid: str = None, allow_notready = None):
     user = ctx.message.author
 
     if raceid == None:
         await bot.send_message(user, "You need to supply the race id to get the multistream link.")
         return
-    link = multistream(raceid)
+    link = multistream(raceid, False if allow_notready is None else True)
     if link == None:
         await bot.say('There is no race with that 5 character id, try remove "srl-" from the room id.')
     else:
         await bot.say(link)
 
 
-
-def multistream(raceid):
+def multistream(raceid, allow_notready: bool):
 
     srl_tmp = r"http://api.speedrunslive.com/races/{}"
     ms_tmp = r"http://multistre.am/{}/"
@@ -78,54 +167,18 @@ def multistream(raceid):
     srl_json = json.load(srlio)
     try:
         entrants = [srl_json['entrants'][k]['twitch'] for k in srl_json['entrants'].keys() if
-                    srl_json['entrants'][k]['statetext'] == "Ready"]
+                    (srl_json['entrants'][k]['statetext'] == "Ready") or allow_notready]
     except KeyError:
         return None
     entrants_2 = r'/'.join(entrants)
     ret = ms_tmp.format(entrants_2)
     return ret
 
-@bot.command()
-async def submit(ctx, runnertime: str = None):
-    if runnertime is None:
-        await ctx.author.send("You must include a time when you submit a time.")
-        await ctx.channel.purge(1)
-        return
 
-    try:
-        # convert to seconds using this method to make sure the time is readable and valid
-        # also allows for time input to be lazy, ie 1:2:3 == 01:02:03 yet still maintain a consistent
-        # style on the leaderboard
-        t = datetime.strptime(runnertime, "%H:%M:%S")
-    except ValueError:
-        await ctx.author.send("The time you provided '" + str(runnertime) +
-                               "', this is not in the format HH:MM:SS (or you took a day or longer)")
-        await ctx.channel.purge(1)
-        return
-
-
-
-    challenge.addtoleaderboard(ctx.author.id,ctx.author.display_name,t,ctx.author.roles)
-    Weeklies[str(ctx.channel.category())].addtoleaderboard(ctx.author.id,ctx.author.display_name,)
-
-
-def run_client(client, *args, **kwargs):
-    loop = asyncio.get_event_loop()
-    while True:
-        try:
-            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "Starting connection")
-            loop.run_until_complete(client.start(*args, **kwargs))
-        except KeyboardInterrupt:
-            loop.run_until_complete(client.logout())
-            break
-        except Exception as e:
-            print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), " Error", e)
-        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "Waiting until restart")
-        time.sleep(Sleep_Time)
 
 
 with open('token.txt', 'r') as f:
     token = f.read()
 token.strip()
 
-run_client(bot, token)
+bot.run(token)
--- weekly.py
+++ weekly.py
@@ -1,32 +1,14 @@
 from datetime import datetime, timedelta
 import json
 
-class Error(Exception):
-   """Base class for other exceptions"""
-   pass
-
-class AddToLeaderboardError(Error):
-    """raised when a player cannot be added to a leaderboard"""
-    pass
-
-class DeleteFromLeaderboardError(Error):
-    """raised when deleting a player from a leaderboard has failed"""
-    pass
-
-class CannotSetVariable(Error):
-    """raised when variable cannot be set"""
-    pass
-
-class CannotGetVariable(Error):
-    """raised when trying to get a variable fails"""
-    pass
-
+from discord.ext import commands
+from discord.utils import get
 
 class Weekly:
     """
     A class that represents a group of channels that is usually used for weekly races (a better name would be helpful)
     """
-    def __init__(self, category = None, role = None, adminrole = None, seedchannel = None, leaderboardchannel = None,
+    def __init__(self, server = None, category = None, role = None, adminrole = None, seedchannel = None, leaderboardchannel = None,
                  spoilerchannel = None):
         self.__category = category
         self.__role = role
@@ -34,7 +16,60 @@ class Weekly:
         self.__seedchannel = seedchannel
         self.__leaderboardchannel = leaderboardchannel
         self.__spoilerchannel = spoilerchannel
+        self.__server = server
         self.__board = dict()
+
+
+    def set(self, variable, value):
+        """
+        set variables for this Weekly
+        :param variable: str of the variable to set
+        :param value: value for that variable
+        :return: None
+        """
+        if variable == "category":
+            self.__category = value
+        elif variable == "seedchannel":
+            self.__seedchannel = value
+        elif variable == "leaderboard":
+            self.__spoilerchannel = value
+        elif variable == "spoilerchannel":
+            self.__leaderboardchannel = value
+        elif variable == "adminrole":
+            self.__adminrole = value
+        elif variable == "board_num":
+            self.__board["num"] = value
+        elif variable == "leaderboardmessage":
+            self.__board["leaderboardmessage"] = value
+        elif variable == "role":
+            self.__role = value
+        else:
+            raise Exception("Cannot set variable.")
+
+    def get(self, variable):
+        """
+        returns the value of self.__variable
+        :param variable: str of te variable you want
+        :return: value of the variable you want
+        """
+        if variable == "category":
+            return self.__category
+        elif variable == "seedchannel":
+            return self.__seedchannel
+        elif variable == "spoilerchannel":
+            return self.__spoilerchannel
+        elif variable == "leaderboard":
+            return self.__leaderboardchannel
+        elif variable == "adminrole":
+            return self.__adminrole
+        elif variable == "board_num":
+            return self.__board["num"]
+        elif variable == "leaderboardmessage":
+            return self.__board["leaderboardmessage"]
+        elif variable == "role":
+            return self.__role
+        else:
+            raise Exception("Cannot get variable.")
 
     def load(self, category):
         """
@@ -54,7 +89,6 @@ class Weekly:
         except KeyError:
             print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "KeyError when trying to load " + str(category))
 
-
     def save(self):
         """
         saves the stored data for this Weekly to the config file
@@ -64,54 +98,10 @@ class Weekly:
             data = json.load(f)
             f.seek(0)
             data[self.__category] = {"role": self.__role, "adminrole": self.__adminrole, "category": self.__category,
-                                        "seedchannel": self.__seedchannel, "spoilerchannel": self.__spoilerchannel,
-                                        "leaderboardchannel": self.__leaderboardchannel}
+                                     "seedchannel": self.__seedchannel, "spoilerchannel": self.__spoilerchannel,
+                                     "leaderboardchannel": self.__leaderboardchannel}
             json.dump(data, f)
             f.truncate()
-
-
-    def set(self, variable, value):
-        """
-        set variables for this Weekly
-        :param variable: str of the variable to set
-        :param value: value for that variable
-        :return: None
-        """
-        if variable == "category":
-            self.__category = value
-        elif variable == "seedchannel":
-            self.__seedchannel = value
-        elif variable == "leaderboard":
-            self.__spoilerchannel = value
-        elif variable == "spoilerchannel":
-            self.__leaderboardchannel = value
-        elif variable == "role":
-            self.__role = value
-        elif variable == "adminrole":
-            self.__adminrole = value
-        else:
-            raise CannotSetVariable
-
-    def get(self, variable):
-        """
-        returns the value of self.__variable
-        :param variable: str of te variable you want
-        :return: value of the variable you want
-        """
-        if variable == "category":
-            return self.__category
-        elif variable == "seedchannel":
-            return self.__seedchannel
-        elif variable == "leaderboard":
-            return self.__spoilerchannel
-        elif variable == "spoilerchannel":
-            return self.__leaderboardchannel
-        elif variable == "role":
-            return self.__role
-        elif variable == "adminrole":
-            return self.__adminrole
-        else:
-            return CannotGetVariable
 
 
     def saveboard(self):
@@ -134,7 +124,7 @@ class Weekly:
                           name if name != None else self.__board["name"], 'r+') as f:
             self.__board = json.load(f)
 
-    def initializeleaderboard(self, name, leaderboardnum = 1, *args):
+    def initializeleaderboard(self, name, leaderboardmessage, roles, leaderboardnum = 1, *args):
         """
         initializes the leaderboard and creates as many sub categories as needed, a name and role is needed for each
         :param name: name of the leaderboard
@@ -144,64 +134,107 @@ class Weekly:
             to make the condition if a player doesnt have that role instead of if they have it
         :return: None
         """
+        if self.__adminrole not in roles:
+            return #TODO make this return something so a message can be sent to the user of the command
         self.__board = dict()
         self.__board["name"] = name
         self.__board["num"] = leaderboardnum
+        self.__board["leaderboardmessage"] = leaderboardmessage
         for i in range(leaderboardnum):
             self.__board[str(i)] = dict()
-            self.__board[str(i)]["title"] = args[2*i]
-            self.__board[str(i)]["role"] = args[2*i+1]
+            self.__board[str(i)]["title"] = args[3*i]
+            self.__board[str(i)]["role"] = args[3*i+1]
+            self.__board[str(i)]["updatable"] = args[3*i+2]
             self.__board[str(i)]["times"] = []
             self.__board[str(i)]["forfeits"] = 0
+            self.__board[str(i)]["participants"] = 0
+            self.__board[str(i)]["requiredrole"] = None
+            self.__board[str(i)]["spectators"] = []
+            self.__board[str(i)]["forfeiters"] = []
+
         self.saveboard()
 
-    def wipeleaderboard(self, name):
+    def wipeleaderboard(self, name, leaderboardmessage, roles, *args):
         """
         wipes the leaderboard of times and renames the whole thing, the amount of leaderboards and each name
             for the specific ones stays the same
         :param name: Name for the new leaderboard
         :return: None
         """
+        if self.__adminrole not in roles:
+            return #TODO make this return something so a message can be sent to the user of the command
         self.__board["name"] = name
+        self.__board["leaderboardmessage"] = leaderboardmessage
         for i in range(self.__board["num"]):
             self.__board[str(i)]["times"] = []
             self.__board[str(i)]["forfeits"] = 0
+            self.__board[str(i)]["participants"] = 0
+            if bool(args[i]):
+                self.__board[str(i)]["updateable"] = True
+            else:
+                self.__board[str(i)]["updateable"] = False
+        self.saveboard()
 
-    def addtoleaderboard(self, id, name, time, roles):
+    def addtoleaderboard(self, id, name, time, roles, board_num = 0):
         """
-        adds a player time to the leaderboard based on the role matching or not
-        ":param id: player unique id
+        adds (or updates) a player time to the leaderboard based on the board number given
+        :param id: player unique id
         :param name: player display name
         :param time: player time
         :param roles: roles that the player has
-        :param matchrole: true if the role must match, false if not
+        :param board_num: the board number, + boards etc will be below in the same message (this might need to be changed
+            to a separate message in the future
         :return: None
         """
         key = None
-        for i in range(self.__board["num"]):
-            if (self.__board[str(i)]["role"] in roles) ^ (not self.__board[]):
-                key = str(i)
-                break
+        if (self.__board[board_num]["role"] not in roles) or (self.__board[board_num]["updatable"]):
+            if (self.__board[board_num]["requiredrole"] == None) or (self.__board[board_num]["requiredrole"] in roles):
+                key = board_num
         if key == None:
-            raise AddToLeaderboardError
-        self.__board[key]["times"].append([id,name,time])
+            raise Exception("Cannot add to leaderboard.")
+        updated = False
+        for i in range(len(self.__board[str(board_num)]["times"])):
+            if self.__board[str(board_num)]["times"][i] == id:
+                self.__board[str(board_num)]["times"][i][2] = time
+                updated = True
+        if not updated:
+            self.__board[key]["times"].append([id,name,time])
         self.__board[key]["times"].sort(
             key=lambda x: datetime.strptime(x[2].strip(), "%H:%M:%S"))
+        self.__board["participants"] += 1
         self.saveboard()
+        return self.__board[board_num]["role"]
 
-    def removefromleaderboard(self, id, leaderboardnum = 0):
+
+
+    def removefromleaderboard(self, id, roles, board_num = 0):
         """
-        removes a player from the leaderboard specified by number leaderboardnum
+        removes a player from the leaderboard specified by number board_num
         :param id: player id to remove
-        :param leaderboardnum: leaderboard number to remove the player from
+        :param board_num: board_num number to remove the player from
         :return: None
         """
-        for i in range(len(self.__board[str(leaderboardnum)]["times"])):
-            if self.__board[str(leaderboardnum)]["times"][i] == id:
-                del self.__board[str(leaderboardnum)]["times"][i]
-                return
-        raise DeleteFromLeaderboardError
+        if self.__adminrole not in roles:
+            return #TODO make this return something so a message can be sent to the user of the command
 
+        for i in range(len(self.__board[str(board_num)]["times"])):
+            if self.__board[str(board_num)]["times"][i][0] == id:
+                del self.__board[str(board_num)]["times"][i]
+                self.__board["participants"] -= 1
+                self.saveboard()
+                return self.__board[board_num]["role"]
+
+    def forfeit(self, id, roles, board_num = 0):
+        if self.__board[board_num]["role"] not in roles:
+            self.__board[str(board_num)]["forfeiters"].append(id)
+            self.__board["forfeits"] += 1
+            self.__board["participants"] += 1
+            return self.__board[str(board_num)]["role"]
+
+    def spectate(self, id, roles, board_num=0):
+        if self.__board[board_num]["role"] not in roles:
+            self.__board[str(board_num)]["spectators"].append(id)
+            return self.__board[str(board_num)]["role"]
 
 
     def writeleaderboard(self):
